33fbc953f3a0df59e8d7ed16a36d28d220eea543
Yusufziyasimsek <119485622+Yusufziyasimsek@users.noreply.github.com>
2022-12-30 22:58:28 +0300

    Add files via upload

---
+++ Sayısal Loto/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] sayilar = new int[6];
+int [] kasaSayiları = new int[6];
+int tahmin = 0;
+
+Console.WriteLine("Lütfen 6 adet sayı giriniz");
+Random rnd = new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    kasaSayiları[i] = rnd.Next(1,60);
+    Console.Write("{0} , Sayıyı Giriniz :", (i + i));
+    sayilar[i] = int.Parse(Console.ReadLine()!);
+}
+foreach (var sayi in sayilar)
+{
+    foreach (var kasasayi in kasaSayiları)
+    {
+        tahmin = sayi == kasasayi ? tahmin++ : tahmin;
+    }
+}
+Console.WriteLine("{0} adet doğru tahminde bulundunuz!", tahmin);
+Console.WriteLine("Random Sayılar:");
+for (int i = 0; i < kasaSayiları.Length; i++)
+{
+    Console.Write("{0} ", kasaSayiları[i]);
+}
+    
+
